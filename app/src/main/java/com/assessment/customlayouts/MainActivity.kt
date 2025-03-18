36f7bf7bc88889f81de736e00f53a5772a3fe40e
ezge <ezge.rea@gmail.com>
2025-03-18 13:57:07 +0100

    custom layout syntax

--- app/src/main/java/com/assessment/customlayouts/MainActivity.kt
+++ app/src/main/java/com/assessment/customlayouts/MainActivity.kt
@@ -12,6 +12,40 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.assessment.customlayouts.ui.theme.CustomLayoutsTheme
+import androidx.compose.ui.layout.Layout
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+/*
+custom layout syntax:
+the measurables parameter contains all of the child elements contained
+within the content, while the constraints parameter contains
+the maximum and minimum width and height values allowed for the children.
+
+@Composable
+fun CustomLayout(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+    Layout(modifier = modifier, content = content) { measurables, constraints ->
+        /* map function measures each child. The result is a
+           list of Placeable instances (one for each child)
+           which is then assigned to a variable named placeables .*/
+        val placeables = measurables.map { measurable ->
+            // Measure each children
+            measurable.measure(constraints)
+        }
+        /* the maximum height and width values allowed by the parent. */
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            /* the placeables variable iterates through each child and
+               positions it relative to the default position designated
+               by the parent. */
+            placeables.forEach { placeable -> placeable.placeRelative(x = 0, y = 0)
+        }
+    }
+}
+ */
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +54,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CustomLayoutsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +62,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(modifier: Modifier = Modifier) {
+    Box(modifier) {
+        CascadeLayout(spacing = 20) {
+            Box(modifier = Modifier.size(60.dp).background(Color.Blue))
+            Box(modifier = Modifier.size(80.dp, 40.dp).background(Color.Red))
+            Box(modifier = Modifier.size(90.dp, 100.dp).background(Color.Cyan))
+            Box(modifier = Modifier.size(50.dp).background(Color.Magenta))
+            Box(modifier = Modifier.size(70.dp).background(Color.Green))
+        }
+    }
+}
+
+@Composable
+fun CascadeLayout(modifier: Modifier = Modifier,
+                  spacing: Int = 0,
+                  content: @Composable () -> Unit
+) {
+    Layout(modifier = modifier, content = content) { measurables, constraints ->
+        var indent = 0
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            var yCoord = 0
+            val placeables = measurables.map { measurable ->
+                measurable.measure(constraints)
+            }
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = indent, y = yCoord)
+                indent += placeable.width + spacing
+                yCoord += placeable.height + spacing
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CustomLayoutsTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
